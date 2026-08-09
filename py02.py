464bef5c55dd404e4d9cc11bd2d84d9fb4b21460
Matheus Tavares <rm566844@fiap.com.br>
2026-08-09 20:27:45 -0300

    aprendendo sobre sintanxe e blocos

---
+++ py02.py
@@ -0,0 +1,16 @@
+# Meu primeiro comentario
+"""
+ESSE é meu segundo comentario
+"""
+
+nome = "Matheus " + "Tavares"
+
+print(nome)
+
+idade = 25
+
+if idade < 18:
+    print("menor de idade")
+else:
+    print("menor de idade")
+    
