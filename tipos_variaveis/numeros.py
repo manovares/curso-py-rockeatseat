7928332aca1620c9e1bc32354a6f48be70c2952f
Matheus Tavares <rm566844@fiap.com.br>
2026-08-09 22:27:20 -0300

    aprendendo sohre usos em operacoes aritimeticas

--- tipos_variaveis/numeros.py
+++ tipos_variaveis/numeros.py
@@ -8,3 +8,36 @@ print("Real com ponto flutuante = ", numero_real)
 
 # type()
 print("Tipo da variavel inteiro = ", type(numero_inteiro))
+
+#soma 
+soma = 1 + 1
+print("1 + 1 =", soma)
+
+#subtracao
+subtracao = 1 - 1
+print("1 - 1 =", subtracao)
+
+# multiplicao
+multiplicacao = 2 * 2
+print("2 x 2 -", multiplicacao)
+
+
+#divisao
+divisao = 5 / 2 
+print("5 / 2 =", divisao)
+print("Tipo da variavel do resultado da divisao = ", type(divisao))
+
+#int
+print(int("Valor em inteiro = ", divisao))
+
+#float
+print(float("Valor em float =  ", divisao))
+
+#modulo %
+modulo = 5 % 2
+print("Modulo = ", modulo)
+
+#divisao inteira
+divisao = 5 // 2 
+print("5 / 2 =", divisao)
+print("Tipo da variavel do resultado da divisao = ", type(divisao))
