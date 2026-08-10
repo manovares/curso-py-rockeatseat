d2b593e2dd68790786b657b95addbce87201c89a
Matheus Tavares <rm566844@fiap.com.br>
2026-08-10 14:17:33 -0300

    aprendendo sobre booleans

---
+++ booleans.py
@@ -0,0 +1,32 @@
+#Condicao verdadeira
+True
+
+#Condição falsa
+False
+
+#Condicionais
+if True:
+    print("Bloco IF vai ser executado")
+else:
+    print("Bloco Else nao sera executado")
+
+#Operadores logicos: and e or
+
+if True and True:
+    print("Bloco sera executado")
+
+if True and False:
+    print("Bloco nao sera executado")
+
+if False and False:
+    print("Bloco nao sera executado")
+
+#OR
+if True or False:
+    print("Bloco OR não vai ser executado")
+
+if False or False:
+    print("Bloco OR não vai ser executado")
+
+if True or True:
+    print("Bloco OR vai ser executado")
