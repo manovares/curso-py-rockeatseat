ab4f13d2d80fca79330ec4e011b103447b3593ed
Matheus Tavares <rm566844@fiap.com.br>
2026-08-09 22:42:25 -0300

    aprendendo sobre formataçao

--- tipos_variaveis/texto.py
+++ tipos_variaveis/texto.py
@@ -4,11 +4,22 @@ nome_completo = "Matheus Tavares"
 nome_completo_aspas = """Matheus
 Tavares"""
 
-numero_completo_quebra = "Matheus \
+nome_completo_quebra = "Matheus \
     Tavares"
 
 nome = "Matheus"
 
 sobrenome = "Tavares"
 
+#Formatação
 
+print("Nome completo (1a forma):", nome_completo)
+print("Nome completo (2a forma):" + nome_completo)
+print("Nome completo (3a forma):" + "Matheus" + "Tavares")
+print("Nome completo (4a forma):" + "Matheus", "Tavares")
+print("Nome completo (5a forma):", nome_completo_aspas)
+print("Nome completo (6a forma):", nome_completo_quebra)
+print("Nome completo (7a forma): %s"  % nome_completo)
+print("Nome completo (8a forma): %s %s"  % (nome, sobrenome))
+print(f"Nome completo (9a forma): {nome} {sobrenome}")
+print("Nome completo (10a forma): {} {}".format(nome, sobrenome))
