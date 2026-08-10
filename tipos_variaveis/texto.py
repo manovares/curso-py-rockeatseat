4d647e6e3dca6526506803eb15ebffe236a4ba18
Matheus Tavares <rm566844@fiap.com.br>
2026-08-09 22:33:17 -0300

    aprendendo sobre textos

---
+++ tipos_variaveis/texto.py
@@ -0,0 +1,14 @@
+# Declaração 
+nome_completo = "Matheus Tavares"
+
+nome_completo_aspas = """Matheus
+Tavares"""
+
+numero_completo_quebra = "Matheus \
+    Tavares"
+
+nome = "Matheus"
+
+sobrenome = "Tavares"
+
+
