53d5d6541b644beba35fe21ced6e7453d28f1d7e
Matheus Tavares <rm566844@fiap.com.br>
2026-08-17 12:28:31 -0300

    tupla indice

---
+++ tipos_variaveis/tupla.py
@@ -0,0 +1,14 @@
+#Criando uma tupla
+minha_tupla = (1,2,2,3,4)
+
+print("Minha tupla:", minha_tupla)
+print("Minha tupla:", minha_tupla[0])
+print("Minha tupla:", minha_tupla[1])
+print("Minha tupla:", minha_tupla[-1])
+
+#Metodo count
+contagem = minha_tupla.count(2)
+print("Quantidade de vezes que o elemento 2 aparece:", contagem)
+
+indice = minha_tupla.index(3)
+print("Indice: ", indice)
