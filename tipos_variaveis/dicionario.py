9ada05ed8c7fbda9f8c1c70bdf6b215a56ce9e05
Matheus Tavares <rm566844@fiap.com.br>
2026-08-17 12:37:31 -0300

    aprendendoi sobre dicionarios

---
+++ tipos_variaveis/dicionario.py
@@ -0,0 +1,11 @@
+# Criando um dicionario de exemplo
+pessoa = {"nome": "João", "idade": 30, "cidade": "São Paulo"}
+
+print("Meu dicionario: ", pessoa)
+
+print("Nome:", pessoa["nome"])
+print("Idade:", pessoa["idade"])
+print("Cidade:", pessoa["cidade"])
+
+print["sobrenome"] = "Silva"
+print("Sobrenome:", pessoa["sobrenome"])
