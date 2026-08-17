8463231129145a3d02c7f1843bdc83ad8950cc59
Matheus Tavares <rm566844@fiap.com.br>
2026-08-17 13:35:07 -0300

    metodos dicionario

--- tipos_variaveis/dicionario.py
+++ tipos_variaveis/dicionario.py
@@ -7,5 +7,30 @@ print("Nome:", pessoa["nome"])
 print("Idade:", pessoa["idade"])
 print("Cidade:", pessoa["cidade"])
 
-print["sobrenome"] = "Silva"
+pessoa["sobrenome"] = "Silva"
 print("Sobrenome:", pessoa["sobrenome"])
+print("Meu dicionario: ", pessoa)
+
+
+pessoa["idade"] = 31
+print("Idade atualizada:", pessoa["idade"])
+
+# Removendo um par chave p valor
+del pessoa["sobrenome"]
+
+print("Meu dicionario de exemplo: ", pessoa)
+
+#Metodos: keys(), values(), items()
+
+chaves = list(pessoa.keys())
+print("Chaves do dicionario: ", chaves)
+print("Primeira chave:", chaves[0])
+
+
+valores = list(pessoa.values())
+print("Valores do dicionarios:", valores)
+print("Primeiro valor do dicionario:", valores[0])
+
+itens = list(pessoa.items())
+print("Pares chave-valor do dicionario:", itens)
+print("Primeiro valor:", itens[0][1])
