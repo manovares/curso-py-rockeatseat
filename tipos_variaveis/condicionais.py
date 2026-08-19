27f913fca8a367990a1fc0daa2062c5f9538235b
Matheus Tavares <rm566844@fiap.com.br>
2026-08-19 11:23:47 -0300

    aprendendo sobre condicionais

---
+++ tipos_variaveis/condicionais.py
@@ -0,0 +1,15 @@
+# if, elif e else
+
+
+#Exemplo de "if"
+idade = 19
+print("Exemplo de comando if")
+if idade >= 18:
+    print("Voce é maior de idade.")
+elif idade >= 12:
+    print("Voce é um adolescente")
+else:
+    print("Voce é menor de idade")
+
+mensagem = "Pode tirar a carteira de habilitaçao" if idade >= 18 else "Voce nao pode tirar a carteira de habilitaçao"
+print(mensagem)
