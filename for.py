8f2a84d83b4b2c20275a73b8dcc5b08a736fd32d
Matheus Tavares <rm566844@fiap.com.br>
2026-08-19 11:45:03 -0300

    for range, enumerate, len

--- for.py
+++ for.py
@@ -20,3 +20,25 @@ for valor in pessoa.values():
 
 for chave, valor in pessoa.items():
     print(f"{chave}: {valor}")
+
+#range
+print("\n Utilizando a função range()")
+for numero in range(5):
+    print("Numero:", numero)
+
+print("\n Utilizando a função range() com len()")
+lista = [1,2,3,4,5]
+print(lista)
+for indice in range(0, len(lista)):
+    if indice == 3:
+        lista[indice] = 5
+    else:
+        lista[indice] = 0
+print(lista)
+
+#enumerate()
+lista_enumerate = ["a", "b", "c"]
+for indice, valor in enumerate(lista_enumerate):
+    print(f"{indice}: {valor}")
+    if indice == 1:
+        print("Indice 1")
