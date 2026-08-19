edf596753971c5a5d0e3ffebae8e4e28daf3a94d
Matheus Tavares <rm566844@fiap.com.br>
2026-08-19 11:53:56 -0300

    fazendo projeto crud rockseat

---
+++ tipos_variaveis/projeto/gerenciador.py
@@ -0,0 +1,15 @@
+while(True):
+    print("\n Menu do gerenciador de lista de tarefas:")
+    print("")
+    print("2. Ver Tarefas")
+    print("3. Atualizar Tarefa")
+    print("4. Completar Tarefa")
+    print("5. Deletar tarefas completadas")
+    print("6. Sair")
+
+    escolha = input("Digite a sua escolha: ")
+
+    if escolha == "6":
+        break
+
+print("Programa finalizado")
